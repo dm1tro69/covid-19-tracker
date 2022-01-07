88ea392f95ae5ec958b7bb9ca91beffa9a970172
dm1tro69 <dimolg22@gmail.com>
2022-01-07 22:18:46 +0200

    add css Cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -3,6 +3,7 @@ import styles from './Cards.module.css'
 import {Card, CardContent, Typography, Grid} from "@material-ui/core";
 import CountUp from "react-countup";
 import {Data} from "../../types";
+import cx from 'classnames'
 
 
 interface Props {
@@ -14,7 +15,7 @@ const Cards:FC<Props> = ({data}) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justifyContent={'center'}>
-                 <Grid item component={Card}>
+                 <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.infected])}>
                      <CardContent>
                           <Typography color={'textSecondary'} gutterBottom>
                                 Infected
@@ -24,17 +25,18 @@ const Cards:FC<Props> = ({data}) => {
                                 start={0}
                                 end={data.confirmed.value}
                                 duration={2.5}
+                                separator={','}
                             />
                          </Typography>
                          <Typography color={'textSecondary'}>
-                             {data.lastUpdate}
+                             {new Date(data.lastUpdate).toDateString()}
                          </Typography>
                          <Typography variant={'body2'}>
                              Number of active cases of COVID-19
                          </Typography>
                      </CardContent>
                  </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.recovered])}>
                     <CardContent>
                         <Typography color={'textSecondary'} gutterBottom>
                             Recovered
@@ -44,18 +46,19 @@ const Cards:FC<Props> = ({data}) => {
                                 start={0}
                                 end={data.recovered.value}
                                 duration={2.5}
+                                separator={','}
                             />
 
                         </Typography>
                         <Typography color={'textSecondary'}>
-                            {data.lastUpdate}
+                            {new Date(data.lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant={'body2'}>
                             Number of recoveries from COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.deaths])}>
                     <CardContent>
                         <Typography color={'textSecondary'} gutterBottom>
                             Deaths
@@ -65,11 +68,12 @@ const Cards:FC<Props> = ({data}) => {
                                 start={0}
                                 end={data.deaths.value}
                                 duration={2.5}
+                                separator={','}
                             />
 
                         </Typography>
                         <Typography color={'textSecondary'}>
-                            {data.lastUpdate}
+                            {new Date(data.lastUpdate).toDateString()}
                         </Typography>
                         <Typography variant={'body2'}>
                             Number of deaths caused by COVID-19
